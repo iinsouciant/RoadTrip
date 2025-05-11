eb17cf07081a37b4f01c482119ffa71c4ac73d17
Ryan Sands <sandsryanj@gmail.com>
2025-05-11 09:28:14 -0700

    drag and drop multiple pins

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
-from tkinter import PhotoImage, Tk, Label
-# from PIL import Image, ImageTk
+from tkinter import PhotoImage, Tk, Label, Event
+from PIL import Image, ImageTk
 import customtkinter as ctk
 import pywinstyles
 
+class PinImage(ImageTk.PhotoImage):
+    def __init__(self):
+        pinPath = './pin.png'
+        self.pinSize = (20, 35)
+        super().__init__(Image.open(pinPath).resize(self.pinSize))
+
 class PinCanvas(ctk.CTkCanvas):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, font, **kwargs):
         super().__init__(master, **kwargs)
         self.configure(bg="#000001", bd=0, highlightthickness=0, relief='ridge', )
         self.grid(row=0, column=2, padx=0, pady=0, rowspan=3, sticky="nsew")
         pywinstyles.set_opacity(self, color="#000001")
         # // maybe move this out of frame and make transparent so then the corners are rounded again
         # Pin Stuff
-        pinPath = './pin.png'
-        pinSize = (20, 35)
+        self.pinImg = PinImage()
         self.locations = {}
+        self.locations[self.create_image(30,30,image=self.pinImg)] = (30,30)
+        self.locations[self.create_image(30,35,image=self.pinImg)] = (30,35)
+
+        self.pinCoords = ctk.CTkLabel(self, text="X: None Y: None", font=font, text_color="black")
+        self.pinCoords.pack(padx="10px", pady="10px", anchor="se")
+
+        # left click and move show coordinates
+        #since it is bound to selected pin, it only happens if click on pin
+        for pin in list(self.locations):
+            # use default arguments in order to know which pin called this function since bind only does event
+            def moveHandler(event, self=self, pin=pin):
+                return self.__movePin(event, pin)
+            def offsetHandler(event, self=self, pin=pin):
+                return self.__setDragOffset(event, pin)
+            self.tag_bind(pin, "<Button1-Motion>", self.updateCoordText, add="+")
+            self.tag_bind(pin, "<Button1-Motion>", moveHandler, add="+")
+            self.tag_bind(pin, "<Button-1>", offsetHandler, add="+")
+
+        self.printWidgetType = True
+
+    def __movePin(self, event:Event, pin):
+        """ 
+        can use lambda or local function handler to take arguments
+        https://stackoverflow.com/questions/4299145/getting-the-widget-that-triggered-an-event
+        https://stackoverflow.com/questions/7299955/tkinter-binding-a-function-with-arguments-to-a-widget
+        """
+        if self.printWidgetType:
+            print(f"event widget type: {type(event.widget)}")
+            self.printWidgetType = False
+        newLoc = (self.canvasx(event.x-self.__dragOffset[0]),self.canvasy(event.y-self.__dragOffset[1]))
+
+        self.moveto(pin,newLoc[0], newLoc[1])
+        # update self.locations to new
+        self.locations[pin] = newLoc
+
+    def updateCoordText(self, event:Event):
+        self.pinCoords.configure(text=f"X: {event.x-self.__dragOffset[0]} Y:{event.y-self.__dragOffset[1]}")
+
+    def __setDragOffset(self, event:Event, pin):
+        """ Get mouse location offset from top left corner when they start dragging"""
+        loc = self.locations[pin]
+        self.__dragOffset = (event.x-loc[0],event.y-loc[1])
+
 
 class PinFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
+        """Frame just to color back area of container and round the corners"""
         super().__init__(master, **kwargs)
         self.configure(fg_color="#a8c7a1", bg_color="transparent")
         self.grid(row=0, column=2, padx=0, pady=0, rowspan=3, sticky="nsew")
@@ -36,7 +85,7 @@ class ContainerFrame(ctk.CTkFrame):
         self.grid_rowconfigure(2,weight=1)
 
         self.pinFrame = PinFrame(self)
-        self.pinCanvas = PinCanvas(self)
+        self.pinCanvas = PinCanvas(self, font)
         # create header
         headerFontSize = font[1] * 2.5 // 1
         self.title = ctk.CTkLabel(self, text="Road Trip Plotter", font=(font[0], headerFontSize), justify="left", anchor="sw", corner_radius=3,)
@@ -48,9 +97,9 @@ class ContainerFrame(ctk.CTkFrame):
         self.body.grid(row=1, column=0, padx=10, pady=(0, 10), columnspan=2, sticky="new")
         # create submit and reset button
         self.submitButton = ctk.CTkButton(self,text="Submit", command=self.submit_callback)
-        self.submitButton.grid(row=2, column=0, padx=2, pady=5,  sticky="sew")
+        self.submitButton.grid(row=2, column=0, padx=(5, 2), pady=5,  sticky="sew")
         self.resetButton = ctk.CTkButton(self,text="Reset", command=self.reset_callback)
-        self.resetButton.grid(row=2, column=1, padx=2, pady=5,  sticky="sew")
+        self.resetButton.grid(row=2, column=1, padx=(2, 5), pady=5,  sticky="sew")
 
     def submit_callback(self):
         print("submitted")
@@ -85,3 +134,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+References:
+https://www.pythontutorial.net/tkinter/
+https://docs.python.org/3/library/tkinter.html#bindings-and-events
+https://customtkinter.tomschimansky.com/tutorial/grid-system/
+https://stackoverflow.com/questions/4299145/getting-the-widget-that-triggered-an-event
+
+"""
